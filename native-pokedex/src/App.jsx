e37190637c8413f5f7a0c882020f766447e57de3
Ronaldo Cesar Mengato Junior <ronaldo.mengato@gmail.com>
2022-12-04 20:32:05 -0300

    add pokemon detail

--- native-pokedex/src/App.jsx
+++ native-pokedex/src/App.jsx
@@ -1,16 +1,37 @@
 import "./App.css";
+import { Outlet, ReactLocation, Router } from "@tanstack/react-location";
 import { PokemonList } from "./components/pokemon-list/PokemonList";
 import { SearchBox } from "./components/search-box/SearchBox";
 
 import { PokemonProvider } from "./store";
+import { PokemonDetail } from "./components/pokemon-detail/PokemonDetail";
+
+const location = new ReactLocation();
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <SearchBox />
+        <PokemonList />
+      </>
+    ),
+  },
+  {
+    path: "/pokemon/:id",
+    element: <PokemonDetail />,
+  },
+];
 
 function App() {
   return (
     <PokemonProvider>
-      <div className="mx-auto max-w-3xl">
-        <SearchBox />
-        <PokemonList />
-      </div>
+      <Router location={location} routes={routes}>
+        <div className="mx-auto max-w-3xl">
+          <Outlet />
+        </div>
+      </Router>
     </PokemonProvider>
   );
 }
